40bb59bb239a7edf4fb9e5146f6745b30211ea93
ingan <isabelngan27@gmail.com>
2019-09-13 18:22:43 -0700

    adding burger time logo

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
             var object = {
                 burgers: data
             }
-            console.log(object)
+            // console.log(object)
             res.render("index", object);
         })
     });
