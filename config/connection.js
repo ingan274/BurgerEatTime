f6df69ef5375166543621940f46996acb23e4e4a
ingan <isabelngan27@gmail.com>
2019-09-16 18:02:38 -0700

    adding jaws sql

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,16 @@
 // code to connect Node to MySQL
 var mysql = require("mysql2");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+if (process.env.JAWSDB_URL) {
+  var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
 module.exports = connection;
